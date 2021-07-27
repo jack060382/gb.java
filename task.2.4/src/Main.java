822ab2b7de2366b8d8d26d681801195145af7d19
Evgeny Prudnikov <jack060382@mail.ru>
2021-07-27 09:41:48 +0300

    fix

--- task.2.4/src/Main.java
+++ task.2.4/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 2.4 Продвинутые вопросы создания графического интерфейса");
+        
         new ChatWindow();
     }
 }
