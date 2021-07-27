e217c77aed92db187a4dfb336272044474aff672
Evgeny Prudnikov <jack060382@mail.ru>
2021-07-27 19:44:11 +0300

    test.2.4

---
+++ task.2.4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // https://github.com/jack060382/gb.java/pull/2
+        System.out.println("Task 2.4 Продвинутые вопросы создания графического интерфейса");
+
+        new ChatWindow();
+    }
+}
